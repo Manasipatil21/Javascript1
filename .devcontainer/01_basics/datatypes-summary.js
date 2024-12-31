d3798f9f514d3cbb84f65d26c24ac83503f7594a
Manasipatil21 <119027643+Manasipatil21@users.noreply.github.com>
2024-12-31 09:47:33 +0000

    Primitive and non primitive

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -25,3 +25,22 @@ const myFunction = function() {
     console.log("Hello world")
 }
 console.log(typeof myobj)
+
+
+//********************************************************************
+
+// Stack (Primitive)-Copy get,Heap (Non- Primitive)-Referece get
+let myYouTubeName = "Manasip811@gmai.com"
+let anothername = myYouTubeName
+anothername="Manasi"
+console.log(anothername)
+console.log(myYouTubeName)
+let user ={
+    email:"mp7482@gmail.com",
+    upi:"647236"
+}
+console.log(user)
+let usertwo =user
+
+usertwo = "hnjkwehfjkwhf"
+console.log(usertwo)
