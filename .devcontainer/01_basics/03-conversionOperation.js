df311090a84265aa2ad2d5bf22f3960656ee5a51
Manasipatil21 <119027643+Manasipatil21@users.noreply.github.com>
2024-12-31 07:44:24 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/03-conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) 
+
+//"33"=33
+//"33abc"=NaN
+//true = 1; false =0
+
+
+let isloggedIn = ""
+let booleanIsLoggedIn =Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+//1-true  0=false;
+// ""=>false 
+//"hitesh" => true
+
+let number = 33
+let stringnumber = String(number)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
