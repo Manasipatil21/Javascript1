fa1c2a5ee26049c3badc2fb4729e6c8624abcc7e
Manasipatil21 <119027643+Manasipatil21@users.noreply.github.com>
2024-12-31 10:59:46 +0000

    Math and Num

---
+++ .devcontainer/01_basics/05_String.js
@@ -0,0 +1,30 @@
+const name = "manasi"
+const repocount = 5
+//console.log(name +repocount+"Value");  // outdated not use like this
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String ('manasip-ghg-fghfh')
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.charAt(4));
+console.log(gameName.substring());
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+const anotherString =gameName.slice(-8,4)
+console.log(anotherString)
+const newStringOne ="Manasi patil 02"
+console.log(newStringOne.trim())
+console.log(newStringOne.replace())
+const url= "ygqwbrqmr%20hqwhf"
+console.log(url.replace('%20','-'))
+console.log(url)
+console.log(url.includes('ygqf'))
+console.log(newStringOne.split(' '));
+
+
