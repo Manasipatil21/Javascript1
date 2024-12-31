83bbb22998ccc13fa63ed27d52033d81ab863318
Manasipatil21 <119027643+Manasipatil21@users.noreply.github.com>
2024-12-31 11:40:20 +0000

    Array Intro

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+//Array
+const myHeros =   [ "salmaan","Shahrukh"]
+const newArray =   [ 0,1,2,3,4,5]
+const array2 = new Array(1,2,3,4)
+
+console.log(newArray[0])
+console.log(typeof array2 )
+console.log(newArray[1])
+
+
+//Array Methods 
+
+newArray.push()
+console.log(newArray)
