281fb52b8ef6e80d80e5b56d76bef2c65b925fee
Manasipatil21 <119027643+Manasipatil21@users.noreply.github.com>
2024-12-31 07:20:45 +0000

    Datatypes and Variavbles

---
+++ .devcontainer/01_basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountemail = "manasip811@gmail.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+let accountState;
+
+//accountId = 2
+accountemail ="mp3941771@gmail.com"
+accountPassword="43435"
+accountCity ="Pune"
+/*
+prefere to not use var because it change all the variable values at any block
+*/
+
+console.log(accountId);
+console.table([accountId,accountemail,accountPassword,accountCity,accountState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+//alert(3+3) // We are using node.js, not browser
+
+console.log(3+3)
+//ode rediabilty it should be high
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null=>standalone value
+//undefined => if we not assogn the value only crating variable
+//symbol=>unique
+
+//object
+
+console.log(typeof "hitesh")
+//null = null type is object 
+//undefined = undefined type is undefined 
