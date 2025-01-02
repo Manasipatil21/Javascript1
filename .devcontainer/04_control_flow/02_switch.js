39fe2d6c9d42ec88099d2345f8823fce64bbb5a7
Manasipatil21 <119027643+Manasipatil21@users.noreply.github.com>
2025-01-02 13:40:01 +0000

    Added Switch cases and condition

---
+++ .devcontainer/04_control_flow/02_switch.js
@@ -0,0 +1,26 @@
+// switch(key){
+//     case value:
+//         break;
+//         default:
+//             break;  
+// }
+
+const month = "March"
+switch (month) {
+    case "jan":
+        console.log("Janvuary")
+        break;
+        case "feb":
+        console.log("February")
+        break;
+        case "March":
+        console.log("March")
+        break;
+        case "april":
+        console.log("April")
+        break;
+
+    default:
+        console.log("Defualt")
+        break;
+}
